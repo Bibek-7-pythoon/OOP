cec0b0d6b664fbe91ed5e7565f9bdfa0ca18a5a2
Bibek <abibek581@gmail.com>
2024-12-31 14:05:10 +0545

    numpy added

---
+++ OOP.py
@@ -0,0 +1,11 @@
+class Student:
+    Name="Bibek Ale"
+    Age=19
+    Address="Dhading"
+    Gender="Male"
+    def display (self):
+        print(f"{self.Name}, {self.Age}, {self.Address}, {self.Gender}")
+        print(f"{self.Name}, {self.Age}, {self.Address}, {self.Gender}")
+        print(f"{self.Name}, {self.Age}, {self.Address}, {self.Gender}")
+std1=Student()
+std1.display()
